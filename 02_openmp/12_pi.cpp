14e75caaf37d25668f109add0225f72e19d01b6c
leo <leoyuki0219@gmail.com.com>
2026-04-13 12:16:29 +0900

    hw: homework1

--- 02_openmp/12_pi.cpp
+++ 02_openmp/12_pi.cpp
@@ -3,10 +3,25 @@
 int main() {
   int n = 10;
   double dx = 1. / n;
-  double pi = 0;
-  for (int i=0; i<n; i++) {
+  double pi_int[n], tmp[n];
+  double *pi = pi_int + n - 1;
+  for (int i=0; i<n; i++)
+  {
     double x = (i + 0.5) * dx;
-    pi += 4.0 / (1.0 + x * x) * dx;
+    pi_int[i] = 4.0 / (1.0 + x * x) * dx;
   }
-  printf("%17.15f\n",pi);
+#pragma omp parallel
+  {
+    for (int j=1; j<n; j<<=1)
+    {
+#pragma omp for
+      for(int i=0; i<n; i++)
+	tmp[i] = pi_int[i];
+
+#pragma omp for
+      for(int i=j; i<n; i++)
+	pi_int[i] += tmp[i-j];
+    }
+  }
+  printf("%17.15f\n",*pi);
 }
